e8bda86bc0d791ac6a4a5c011bc2277fca5abde5
Willy Douglas <willygdouglas@gmail.com>
2016-04-17 12:01:21 -0600

    WIP getting the dev server to work with loopback

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:4000',
+    'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
     './client/index'
   ],
@@ -20,7 +20,7 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
+      include: path.join(__dirname, 'client')
     }]
   }
 };
