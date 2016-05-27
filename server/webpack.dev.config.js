460fb01708a343eec997a40dea8cc68ca0c62ef3
Derek Koloditch <derek.koloditch@gmail.com>
2016-05-27 10:45:57 -0600

    vagrant webpack hot reload bug fix

--- server/webpack.dev.config.js
+++ server/webpack.dev.config.js
@@ -8,6 +8,11 @@ module.exports = {
     'webpack/hot/only-dev-server',
     path.resolve(__dirname, '../client/src/index')
   ],
+  devServer: {
+    inline: true, // reload on the fly (auto refresh)
+    host: "0.0.0.0", // binds to localhost for browser viewing
+    port: 8080 // which port to run the server on
+  },
   output: {
     path: path.resolve(__dirname, '../client/dist'),
     filename: 'bundle.js',
